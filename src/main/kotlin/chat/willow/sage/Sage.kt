b32390ca6d05c49ac3a4affa2fbced2953d5dabc
Sky Welch <git@bunnies.io>
2017-03-16 20:02:15 +0000

    Basic client connection

--- src/main/kotlin/chat/willow/sage/Sage.kt
+++ src/main/kotlin/chat/willow/sage/Sage.kt
@@ -1,6 +1,8 @@
 package chat.willow.sage
 
 import chat.willow.sage.helper.loggerFor
+import chat.willow.warren.WarrenClient
+import chat.willow.warren.event.ChannelMessageEvent
 
 object Sage {
 
@@ -8,6 +10,28 @@ object Sage {
 
     @JvmStatic fun main(args: Array<String>) {
         LOGGER.info("hello, sage!")
+
+        if (args.size < 3) {
+            LOGGER.error("Usage: server user #comma,#separated,#channels")
+            return
+        }
+
+        val argServer = args[0]
+        val argUser = args[1]
+        val argChannels = args[2].split(delimiters = ',')
+
+        val irc = WarrenClient.build {
+            server(argServer)
+            user(argUser)
+
+            argChannels.forEach { channel(it) }
+        }
+
+        irc.events.on(ChannelMessageEvent::class) {
+            LOGGER.info("channel message: $it")
+        }
+
+        irc.start()
     }
 
 }
